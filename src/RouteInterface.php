89df5b1ca1e239ef56da825895cb5c54e7054f56
Daniel Olfelt <dolfelt@gmail.com>
2015-05-24 15:30:31 -0500

    Filling out the RouteInteface a bit more.

--- src/RouteInterface.php
+++ src/RouteInterface.php
@@ -7,4 +7,23 @@ namespace Spark\Adr;
  */
 interface RouteInterface
 {
+
+    /**
+     * Returns a reference to a class for the domain/application service logic
+     * @return mixed
+     */
+    public function getDomain();
+
+    /**
+     * Returns a reference to a class for sending the payload to the server
+     * @return mixed
+     */
+    public function getResponder();
+
+    /**
+     * Returns a reference to a class for parsing HTTP input to be sent to the application service (domain)
+     * @return mixed
+     */
+    public function getInput();
+
 }
