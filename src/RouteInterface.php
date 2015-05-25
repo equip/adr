89bce9de48ee668a7705ac222d5bb1a85f201e69
Woody Gilk <woody.gilk@gmail.com>
2015-05-24 20:18:07 -0500

    Correct the route method return types

--- src/RouteInterface.php
+++ src/RouteInterface.php
@@ -2,28 +2,20 @@
 
 namespace Spark\Adr;
 
-/**
- * Value object that carries the route to the action.
- */
 interface RouteInterface
 {
-
     /**
-     * Returns a reference to a class for the domain/application service logic
-     * @return mixed
+     * @return DomainInterface
      */
     public function getDomain();
 
     /**
-     * Returns a reference to a class for sending the payload to the server
-     * @return mixed
-     */
-    public function getResponder();
-
-    /**
-     * Returns a reference to a class for parsing HTTP input to be sent to the application service (domain)
-     * @return mixed
+     * @return InputInterface
      */
     public function getInput();
 
+    /**
+     * @return ResponderInterface
+     */
+    public function getResponder();
 }
